96eb60272b7d63c11bbc5926453595cb4768ce1f
Mateusz Charewicz <mateusz.charewicz@microsoft.wsei.edu.pl>
2022-06-25 22:30:49 +0200

    Add project files.

---
+++ RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.Entities
+{
+    public class RestaurantDbContext : DbContext
+    {
+        private string _connectionString =
+            "Server=(localdb)\\mssqllocaldb;Database=RestaurantDB;Trusted_Connection=True";
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>()
+                .Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<Dish>()
+                .Property(x => x.Name)
+                .IsRequired();
+            modelBuilder.Entity<Address>()
+                .Property(x => x.City)
+                .IsRequired()
+                .HasMaxLength(50);
+            modelBuilder.Entity<Address>()
+                .Property(x => x.Street)
+                .IsRequired()
+                .HasDefaultValue(50);
+                
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+        public DbSet<RestaurantAPI.Models.RestaurantDto> RestaurantDto { get; set; }
+    }
+}
---
+++ RestaurantAPI/RestaurantSeeder.cs
@@ -0,0 +1,71 @@
+﻿using RestaurantAPI.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestaurantAPI
+{
+    public class RestaurantSeeder
+    {
+        private readonly RestaurantDbContext _dbContext;
+        public RestaurantSeeder(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if(_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    _dbContext.Restaurants.AddRange(restaurants);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            var restaurants = new List<Restaurant>()
+            {
+
+
+                new Restaurant
+                {
+                        Name = "KFC",
+                        Category = "Fast Food",
+                        Description = "KFC (Kentucky Fried Chicken) is an       American fast food restaurant chain headquartered in Louisville, Kentucky, that specializes in fried chicken.",
+                        HasDelivery = true,
+                        ContactEmail = "contact@kfc.com",
+                        ContactNumber = "555444333",
+                        Dishes = new List<Dish>
+                        {
+                            new Dish { Name = "Twister", Price = 10.30M},
+                            new Dish { Name = "Zinger", Price = 9.30M}
+                        },
+                        Address = new Address { City = "Kraków", Street = "Długa 5", PostalCode = "30-001"}
+
+                 },
+                new Restaurant
+                {
+                        Name = "McDonald's",
+                        Category = "Fast Food",
+                        Description = "McDonald's Corporation is an American-based multinational fast food chain, founded in 1940 as a restaurant operated by Richard and Maurice McDonald, in San Bernardino, California, United States.",
+                        HasDelivery = true,
+                        ContactEmail= "mcdonald@mcd.com",
+                        ContactNumber= "111222333",
+                        Dishes = new List<Dish>
+                        {
+                            new Dish { Name = "Cheeseburger", Price = 3.30M},
+                            new Dish { Name = "McFlurry", Price = 7.30M}
+                        },
+                        Address = new Address { City = "Kraków", Street = "Długa 10", PostalCode = "30-001"}
+
+                 }
+            };
+            return restaurants;
+
+        }
+    }
+}
